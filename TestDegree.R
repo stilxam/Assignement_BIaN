c13d2c93919908515b2b0f1b1d6a0c971cd073b1
felipefbcintra <78799008+felipefbcintra@users.noreply.github.com>
2022-10-19 20:09:38 +0200

    Add files via upload

--- TestDegree.R
+++ TestDegree.R
@@ -3,11 +3,11 @@ library(nstandr)
 library(dplyr)
 library(igraph)
 
-top_100_database <- read.csv(file = "C:/Users/Felipe Cintra/Documents/TUE_2223/JBE140-Bussiness Insights and Networks/RTutorials and Quiz/top_100_database.csv", sep = ",")
-sdc_database <- read.csv(file = "C:/Users/Felipe Cintra/Documents/TUE_2223/JBE140-Bussiness Insights and Networks/RTutorials and Quiz/sdc_database.csv", sep = ",", quote = "")
+top_100_database <- read.csv(file = "C:/Users/Felipe Cintra/Documents/TUE_2223/JBE140-Bussiness Insights and Networks/top_100_database.csv", sep = ",")
+sdc_database <- read.csv(file = "C:/Users/Felipe Cintra/Documents/TUE_2223/JBE140-Bussiness Insights and Networks/sdc_database.csv", sep = ",")
 
 sdc_database$date_announced <- substr((sdc_database$date_announced), 1, 4)
-sdc_2019_2021 <- sdc_database[sdc_database$date_announced %in% c('2019', '2020', '2021'), ]
+sdc_2019_2021 <- sdc_database[sdc_database$date_announced %in% c("2020", "2021"), ]
 
 top_100_names <- top_100_database$Company.name.Latin.alphabet
 
@@ -41,7 +41,10 @@ for (value in db_based_on_deal$deal_number){
   deal_df <- db_based_on_deal[db_based_on_deal$deal_number == value, ]
   deal_participants <- c(deal_df$participants)
   
+  repeats_vector <- c()
+  
   for (name_1 in deal_participants){
+    
     for(name_2 in deal_participants){
       
       if (company_matrix[name_1, name_2] != 0){
@@ -64,7 +67,22 @@ for(row in 1:nrow(company_matrix)){
   }
 }
 
+for(row in 1:nrow(company_matrix)){
+  
+  for(col in 1:ncol(company_matrix)){
+    
+    if (company_matrix[row, col] == 1){
+      
+      company_matrix[col, row] = 0
+    }
+  }
+}
+
+
 company_deals_network <- igraph::graph.incidence(company_matrix)
+
+isolated = which(igraph::degree(company_deals_network)==0)
+company_deals_network_clean = igraph::delete.vertices(company_deals_network, isolated)
 
 #centrality calculations
 g=graph.adjacency(company_matrix)
@@ -78,9 +96,8 @@ colnames(centrality_new)=c('degree','closeness','betweenness')
 # plot(company_deals_network, vertex.size = 1, vertex.label = NA)
 
 
-
 sdc_database$date_announced <- substr((sdc_database$date_announced), 1, 4)
-sdc_2017_2018 <- sdc_database[sdc_database$date_announced %in% c('2017', '2018'), ]
+sdc_2017_2018 <- sdc_database[sdc_database$date_announced %in% c("2017", "2018"), ]
 
 top_100_names <- top_100_database$Company.name.Latin.alphabet
 
@@ -114,7 +131,10 @@ for (value in db_based_on_deal$deal_number){
   deal_df <- db_based_on_deal[db_based_on_deal$deal_number == value, ]
   deal_participants <- c(deal_df$participants)
   
+  repeats_vector <- c()
+  
   for (name_1 in deal_participants){
+    
     for(name_2 in deal_participants){
       
       if (company_matrix[name_1, name_2] != 0){
@@ -137,8 +157,22 @@ for(row in 1:nrow(company_matrix)){
   }
 }
 
+for(row in 1:nrow(company_matrix)){
+  
+  for(col in 1:ncol(company_matrix)){
+    
+    if (company_matrix[row, col] == 1){
+      
+      company_matrix[col, row] = 0
+    }
+  }
+}
+
+
 company_deals_network <- igraph::graph.incidence(company_matrix)
 
+isolated = which(igraph::degree(company_deals_network)==0)
+company_deals_network_clean = igraph::delete.vertices(company_deals_network, isolated)
 #centrality calculations
 g=graph.adjacency(company_matrix)
 degree=degree(g,mode='total',normalized=FALSE)
@@ -154,4 +188,4 @@ colnames(centrality_old)=c('degree','closeness','betweenness')
 centrality_new_median <- apply(centrality_new,2, median)
 centrality_new_means <- colMeans(centrality_new)
 centrality_old_median <- apply(centrality_old,2, median)
-t.test(data.frame(centrality_old)$closeness, data.frame(centrality_new)$closeness)
+t.test(data.frame(centrality_old)$degree, data.frame(centrality_new)$degree)
