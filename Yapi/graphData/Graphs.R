2ac32ae27ad140515ed61226ca1f50940d0bd999
stilxam <m.l.h.litsios@student.tue.nl>
2022-10-19 23:59:19 +0200

    FINAL CODE

--- Yapi/graphData/Graphs.R
+++ Yapi/graphData/Graphs.R
@@ -12,104 +12,146 @@ for(pkg in libs){
 }
 install.packages("ggplot2")
 library("ggplot2")
+
 #graph PostEPS
-EPSpost <- read.csv("EPSpost.csv", header = TRUE)
-rdate <- as.Date(EPSpost$Earnings.Date, "%Y-%m-%d")
 
-AMD <- EPSpost$EPS.AMD
-INTC <- EPSpost$EPS.INTC
-HTHIY <- EPSpost$EPS.HTHIY
-STM <- EPSpost$EPS.STM
-IBM <- EPSpost$EPS.IBM
-TXN <- EPSpost$EPS.TXN
+csvAMD <- read.csv("postAMD.csv", header = TRUE)
+predate <- as.Date(csvAMD$Earnings.Date)
+postAMD<- csvAMD$Reported.EPS
 
-plot(x = rdate, y =AMD,type = "l",
-     col = 1, xlab = "date", ylab = "Earnings Per Share ($)", ylim = c(-5,5), main = "Earning Per Share ($) of Key Chip Manufacturers During COVID-19")
-lines(x = rdate,y =INTC, col=2)
-lines(x = rdate,y =HTHIY, col=3)
-lines(x = rdate,y =STM, col=4)
-lines(x = rdate,y =IBM, col=5)
-lines(x = rdate,y = TXN, col= 6)
-legend(x= "bottomleft", legend = c("AMD", "INTC", "HTHIY", "STM", "IBM", "TXN"), fill = 1:6)
+csvHTHIY <- read.csv("postHTHIY.csv", header = TRUE)
+postHTHIY <- csvHTHIY$Reported.EPS
+
+csvIBM <- read.csv("postIBM.csv", header = TRUE)
+postIBM <- csvIBM$Reported.EPS
+
+csvINTC <- read.csv("postINTC.csv", header = TRUE)
+postINTC <- csvINTC$Reported.EPS
+
+csvMSFT <- read.csv("postMSFT.csv", header = TRUE)
+postMSFT <- csvMSFT$Reported.EPS
+
+csvNVDA <- read.csv("postNVDA.csv", header = TRUE)
+postNVDA <- csvNVDA$Reported.EPS
+
+csvNXPI <- read.csv("postNXPI.csv", header = TRUE)
+postNXPI <- csvNXPI$Reported.EPS
+
+csvSTM <- read.csv("postSTM.csv", header = TRUE)
+postSTM <- csvSTM$Reported.EPS
+
+csvTXN <- read.csv("postTXN.csv", header = TRUE)
+postTXN <- csvTXN$Reported.EPS
+
+
+
+
+plot(x = predate, y =postAMD,type = "l",
+     col = 1, xlab = "date", ylab = "Earnings Per Share ($)", ylim = c(-5,5), main = "Earning Per Share ($) of Key Chip Manufacturers After The start of COVID-19")
+lines(x = predate,y =postHTHIY, col=2)
+lines(x = predate,y =postIBM, col=3)
+lines(x = predate,y =postINTC, col=4)
+lines(x = predate,y =postMSFT, col=5)
+lines(x = predate,y = postNVDA, col= 6)
+lines(x = predate, y = postNXPI, col = 7 )
+lines(x = predate, y = postSTM , col = 8 )
+lines(x = predate, y = postTXN, col = 19)
+legend(x= "bottomright", legend = c("AMD", "HTHIY", "IBM", "INTC", "MSFT", "NVDA", "NXPI", "STM", "TXN"), fill = c(1,2,3,4,5,6,7,8,19))
 
 #EPSPre
-csvAMD <- read.csv("preAMD.csv", header = TRUE)
-predate <- as.Date(csvAMD$Earnings.Date)
-preAMD<- csvAMD$Reported.EPS
-csvINTC <- read.csv("preINTC.csv", header = TRUE)
-preINTC<- csvINTC$Reported.EPS
-csvHTHIY <- read.csv("preHTHIY.csv", header = TRUE)
-preHTHIY<- csvHTHIY$Reported.EPS
-csvSTM <- read.csv("preSTM.csv", header = TRUE)
-preSTM<- csvSTM$Reported.EPS
-csvIBM <- read.csv("preIBM.csv", header = TRUE)
-preIBM<- csvIBM$Reported.EPS
-csvTXN <- read.csv("preTXN.csv", header = TRUE)
-preTXN<- csvTXN$Reported.EPS
+precsvAMD <- read.csv("preAMD.csv", header = TRUE)
+predate <- as.Date(precsvAMD$Earnings.Date)
+preAMD<- precsvAMD$Reported.EPS
 
-plot(x = predate, y = preAMD, type = "l", col = 1,ylim = c(-0.5, 5), xlab = "Date", ylab = "Earnings Per Share ($)", main = "Earnings Per Share ($) of Key Chip Manufactures Pre-COVID19")
-lines(x = predate, y = preINTC, col = 2 )
-lines(x = predate, y = preHTHIY, col = 3 )
-lines(x = predate, y = preSTM, col = 4 )
-lines(x = predate, y = preIBM, col = 5)
-lines(x = predate, y = preTXN, col = 6)
-legend(x = "topright", legend = c("AMD", "INTC", "HTHIY", "STM", "IBM", "TXN"), fill = 1:6)
+precsvHTHIY <- read.csv("preHTHIY.csv", header = TRUE)
+preHTHIY <- precsvHTHIY$Reported.EPS
 
+precsvIBM <- read.csv("preIBM.csv", header = TRUE)
+preIBM <- precsvIBM$Reported.EPS
 
+precsvINTC <- read.csv("preINTC.csv", header = TRUE)
+preINTC <- precsvINTC$Reported.EPS
 
+precsvMSFT <- read.csv("preMSFT.csv", header = TRUE)
+preMSFT <- precsvMSFT$Reported.EPS
 
+precsvNVDA <- read.csv("preNVDA.csv", header = TRUE)
+preNVDA <- precsvNVDA$Reported.EPS
 
+precsvNXPI <- read.csv("preNXPI.csv", header = TRUE)
+preNXPI <- precsvNXPI$Reported.EPS
+
+precsvSTM <- read.csv("preSTM.csv", header = TRUE)
+preSTM <- precsvSTM$Reported.EPS
+
+precsvTXN <- read.csv("preTXN.csv", header = TRUE)
+preTXN <- precsvTXN$Reported.EPS
+
+plot(x = predate, y = preAMD, type = "l", col = 1,ylim = c(-1, 6.5), xlab = "Date", ylab = "Earnings Per Share ($)", main = "Earnings Per Share ($) of Key Chip Manufactures Pre-COVID-19")
+
+lines(x = predate,y =preHTHIY, col=2)
+lines(x = predate,y =preIBM, col=3)
+lines(x = predate,y =preINTC, col=4)
+lines(x = predate,y =preMSFT, col=5)
+lines(x = predate,y = preNVDA, col= 6)
+lines(x = predate, y = preNXPI, col = 7 )
+lines(x = predate, y = preSTM , col = 8 )
+lines(x = predate, y = preTXN, col = 19)
+legend(x= "topright", legend = c("AMD", "HTHIY", "IBM", "INTC", "MSFT", "NVDA", "NXPI", "STM", "TXN"), fill = c(1,2,3,4,5,6,7,8,19))
 
 
 
 # STOCKS pre covid
 closePre <- read.csv("closePre.csv", header = TRUE)
 closePreDate <- as.Date(closePre$Date)
-closePreTOSYY <- closePre$TOSYY.Stock.Value.Change....
-closePreINTC <- closePre$INTC.Stock.Value.Change....
-closePreTXN <- closePre$TXN.Stock.Value.Change....
-closePreSHCAY <- closePre$SHCAY.Stock.Value.Change....
-closePreHTHIY <- closePre$HTHIY.Stock.Value.Change....
-closePreFJTSY <- closePre$FJTSY.Stock.Value.Change....
 closePreAMD <- closePre$AMD.Stock.Value.Change....
-closePreSTM <- closePre$STM.Stock.Value.Change....
+closePreHTHIY <- closePre$HTHIY.Stock.Value.Change....
 closePreIBM <- closePre$IBM.Stock.Value.Change....
+closePreINTC <- closePre$INTC.Stock.Value.Change....
+closePreMSFT <- closePre$MSFT.Stock.Value.Change....
+closePreNVDA <- closePre$NVDA.Stock.Value.Change....
+closePreNXPI <- closePre$NXPI.Stock.Value.Change....
+closePreSTM <- closePre$STM.Stock.Value.Change....
+closePreTXN <- closePre$TXN.Stock.Value.Change....
 
-plot(x = closePreDate, y = closePreTOSYY, type = "l", ylim = c(-100, 200) , col = 1,  xlab = "Date", ylab = "Stock Price Change (%)", main = "Change in Stock Price (%) Before COVID-19" )
-lines(x = closePreDate, y = closePreINTC, col =2)
-lines(x = closePreDate, y = closePreTXN, col =3)
-lines(x = closePreDate, y = closePreSHCAY, col =4)
-lines(x = closePreDate, y = closePreHTHIY, col =5)
-lines(x = closePreDate, y = closePreFJTSY, col =6)
-lines(x = closePreDate, y = closePreAMD, col =7)
+
+
+plot(x = closePreDate, y = closePreAMD, type = "l", ylim = c(-50, 200) , col = 1,  xlab = "Date", ylab = "Stock Price Change (%)", main = "Change in Stock Price (%) Before COVID-19" )
+lines(x = closePreDate, y = closePreHTHIY, col =2)
+lines(x = closePreDate, y = closePreIBM, col =3)
+lines(x = closePreDate, y = closePreINTC, col =4)
+lines(x = closePreDate, y = closePreMSFT, col =5)
+lines(x = closePreDate, y = closePreNVDA, col =6)
+lines(x = closePreDate, y = closePreNXPI, col =7)
 lines(x = closePreDate, y = closePreSTM, col =8)
-lines(x = closePreDate, y = closePreIBM, col =9)
-legend(x ="topleft", legend = c("TOSYY", "INTC", "TXN", "SHCAY", "HTHIY", "FJTSY", "AMD", "STM", "IBM"), fill = 1:9)
+lines(x = closePreDate, y = closePreTXN, col =9)
+legend(x= "topleft", legend = c("AMD", "HTHIY", "IBM", "INTC", "MSFT", "NVDA", "NXPI", "STM", "TXN"), fill = c(1,2,3,4,5,6,7,8,19))
+
 
 
 #Stocks Post Covid
 closePost <- read.csv("closePost.csv", header = TRUE)
 closePostDate <- as.Date(closePost$Date)
-closePostTOSYY <- closePost$TOSYY.Stock.Value.Change....
-closePostINTC <- closePost$INTC.Stock.Value.Change....
-closePostTXN <- closePost$TXN.Stock.Value.Change....
-closePostSHCAY <- closePost$SHCAY.Stock.Value.Change....
-closePostHTHIY <- closePost$HTHIY.Stock.Value.Change....
-closePostFJTSY <- closePost$FJTSY.Stock.Value.Change....
 closePostAMD <- closePost$AMD.Stock.Value.Change....
-closePostSTM <- closePost$STM.Stock.Value.Change....
+closePostHTHIY <- closePost$HTHIY.Stock.Value.Change....
 closePostIBM <- closePost$IBM.Stock.Value.Change....
+closePostINTC <- closePost$INTC.Stock.Value.Change....
+closePostMSFT <- closePost$MSFT.Stock.Value.Change....
+closePostNVDA <- closePost$NVDA.Stock.Value.Change....
+closePostNXPI <- closePost$NXPI.Stock.Value.Change....
+closePostSTM <- closePost$STM.Stock.Value.Change....
+closePostTXN <- closePost$TXN.Stock.Value.Change....
 
-plot(x = closePostDate, y = closePostTOSYY, type = "l", ylim = c(-100, 250) , col = 1,  xlab = "Date", ylab = "Stock Price Change (%)", main = "Change in Stock Price (%) During COVID-19" )
-lines(x = closePostDate, y = closePostINTC, col =2)
-lines(x = closePostDate, y = closePostTXN, col =3)
-lines(x = closePostDate, y = closePostSHCAY, col =4)
-lines(x = closePostDate, y = closePostHTHIY, col =5)
-lines(x = closePostDate, y = closePostFJTSY, col =6)
-lines(x = closePostDate, y = closePostAMD, col =7)
+plot(x = closePostDate, y = closePostAMD, type = "l", ylim = c(-50, 250) , col = 1,  xlab = "Date", ylab = "Stock Price Change (%)", main = "Change in Stock Price (%) During COVID-19" )
+lines(x = closePostDate, y = closePostHTHIY, col =2)
+lines(x = closePostDate, y = closePostIBM, col =3)
+lines(x = closePostDate, y = closePostINTC, col =4)
+lines(x = closePostDate, y = closePostMSFT, col =5)
+lines(x = closePostDate, y = closePostNVDA, col =6)
+lines(x = closePostDate, y = closePostNXPI, col =7)
 lines(x = closePostDate, y = closePostSTM, col =8)
-lines(x = closePostDate, y = closePostIBM, col =9)
-legend(x ="topleft", legend = c("TOSYY", "INTC", "TXN", "SHCAY", "HTHIY", "FJTSY", "AMD", "STM", "IBM"), fill = 1:9)
+lines(x = closePostDate, y = closePostTXN, col =9)
+legend(x= "topleft", legend = c("AMD", "HTHIY", "IBM", "INTC", "MSFT", "NVDA", "NXPI", "STM", "TXN"), fill = c(1,2,3,4,5,6,7,8,19))
 
 
 
